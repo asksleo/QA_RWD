ceb0ab8b31f810bf9e8d236d32a32fb72b6d9f3d
karan kumar <karan.kumar.554@gmail.com>
2024-02-15 13:40:12 +0530

    Commit

--- src/test/java/hooks/BeforeandAfter.java
+++ src/test/java/hooks/BeforeandAfter.java
@@ -1,9 +1,6 @@
 package hooks;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
+
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
